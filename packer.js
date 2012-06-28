06c8c79dd2968d36cfa07b1c9f37207591556523
Alex Kojin <alexkojin@gmail.com>
2012-06-28 21:13:02 +0400

    allow pack multiple packages

--- packer.js
+++ packer.js
@@ -2,57 +2,78 @@
 
 var fs = require("fs"),
     path = require('path'),
-    exec = require('child_process').exec;
+    exec = require('child_process').exec,
+    _ = require('underscore');
 
 
 var Packer = function(configPath){
+  var self = this;
+
   var config = null,
       workingDir = '',
-      outputDir = '';
+      outputDir = '',
+      packages = [];
 
   // Read config
-  if(configPath) {
-    config = fs.readFileSync(configPath);
-    config = JSON.parse(config);
-    workingDir = path.dirname(configPath) + '/';
-  } else {
-    config = require('./assets');
-  }
-  console.log(config);
+  config = JSON.parse( fs.readFileSync(configPath || './assets.json') );
+  workingDir = path.dirname(configPath);
+  // console.log(config);
+
+  config.options = config.options || [];
 
   // Make output dir
   if(config.outputDir) {
-    outputDir = workingDir + config.outputDir;
+    outputDir = path.join(workingDir, config.outputDir);
     if(!path.existsSync(outputDir)) fs.mkdirSync(outputDir);
   } else {
     outputDir = workingDir;
   }
 
-  config.options = config.options || [];
 
-  var jsFiles = [];
-  config.js.forEach(function(js){
-    jsFiles.push(workingDir + js);
-  });
-  jsFiles = jsFiles.join(' ');
-  
+  // packages
+  if(config.js) packages.push(config);
 
-  // Prepare command for uglify
-
-  var outputFile = function() {
-    return outputDir + '/' + config.name + "-" + config.version + ".js";
+  if(config.packages) {
+    config.packages.forEach(function(package){
+      var def = _.clone(config);
+      _.extend(def, package);
+      packages.push(def);
+    });
   }
 
+  // console.log(packages);
+  
+  // Prepare command for uglify
+
   this.compress = function(){
-    var command = '"' + __dirname  + '/node_modules/uglify-js/bin/uglifyjs" --output "' +
-              outputFile() + '" --no-copyright ' + jsFiles + ' ' + config.options.join(' ');
-    console.log(command);
+    packages.forEach(function(package){
+      self.compressPackage(package);
+    });
+  };
+
+  this.compressPackage = function(package) {
+    var outputFile = outputDir + '/' + package.name + "-" + package.version + ".js";
+    var files = _.map(package.js, function(js){
+                  return path.join(workingDir, package.sourceDir, js);
+                } ).join(' ');
+
+    var command = 'cat '+ files +' | "' + __dirname  + '/node_modules/uglify-js/bin/uglifyjs" --output "' +
+              outputFile + '" --no-copyright ' + package.options.join(' ');
+    // console.log(command);
 
     // Execute
     exec(command, function (err, stdout, stderr) {
-      console.log(err);
-      console.log(stdout);
-      console.log(stderr);
+      if(err) console.log(err);
+      if(stdout) console.log(stdout);
+      if(stderr) console.log(stderr);
+
+      fs.stat(outputFile, function(err, stat){
+          var red, blue, reset;
+          red   = '\u001b[31m';
+          blue  = '\u001b[34m';
+          reset = '\u001b[0m';
+          console.log(blue,'OK', reset, outputFile, "" + parseInt(stat.size / 1024) + 'KB')  
+      });
     });
   };
 };
